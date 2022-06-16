d25f12123503d0aa663533fb7ef10b56522a54a5
alfredo <alfredneto@hotmail.com>
2022-06-16 20:10:48 -0300

    Setup PostgreSQL

---
+++ app/app/test/tests_calc.py
@@ -0,0 +1,22 @@
+"""
+Sample Tests
+"""
+
+from app import calc
+from django.test import SimpleTestCase
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding number together."""
+        result = calc.add(5, 6)
+
+        self.assertEqual(result, 11)
+
+    def test_subtract_numbers(self):
+        """Test subtracting numbers."""
+        result = calc.subtract(10, 15)
+
+        self.assertEqual(result, -5)
